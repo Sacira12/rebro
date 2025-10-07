667569d62761b3933d34b992bee0b0beb2569635
Daniil Barmenkov <danya.barmenkov89@gmail.com>
2025-10-07 16:00:16 +0500

    changed handlers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import functional_buy
 import functional_man
 import config
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
 # Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
@@ -23,34 +23,49 @@ async def process_start_command(message: Message):
     )
 
 
-# этот хэндлер срабатывает на остальные сообщения
-@dp.message()
-async def managers(message: Message):
-    if message.text == pin:
-        tg_id = message.chat.id
-        flag = bool(message.from_user.username)
-        if flag:
-            name = message.from_user.username
-        else:
-            name = 'not username'
-        t = await functional_man.registration(tg_id, name)
-        await message.answer(text=t)
-    elif message.text.lower() == "увольняюсь":
-        t = await functional_man.delete_man(message.from_user.username)
-        await message.answer(t)
-    elif message.text.lower() == "открыть смену":
-        c = await functional_man.open_status(message.from_user.id)
-        await message.answer(c)
-    elif message.text.lower() == "закрыть смену":
-        c = await functional_man.close_status(message.from_user.id)
-        await message.answer(c)
-    elif message.text.lower() == "готов к покупателям":
-        c = await functional_man.open_state(message.from_user.id)
-        await message.answer(c)
-    elif message.text == '/manager':
-        text = await functional_buy.distribution(message.from_user.id)
-        await message.answer(text)
+@dp.message(Command(commands=pin))
+async def process_register_command(message: Message):
+    tg_id = message.chat.id
+    flag = bool(message.from_user.username)
+    if flag:
+        name = message.from_user.username
+    else:
+        name = 'not username'
+    t = await functional_man.registration(tg_id, name)
+    await message.answer(text=t)
 
+
+@dp.message(Command(commands="delete_me"))
+async def process_delete_command(message: Message):
+    t = await functional_man.delete_man(message.from_user.username)
+    await message.answer(t)
+
+
+@dp.message(Command(commands="open_shift"))
+async def process_open_shift_command(message: Message):
+    c = await functional_man.open_status(message.from_user.id)
+    await message.answer(c)
+
+
+@dp.message(Command(commands='close_shift'))
+async def process_close_shift_command(message: Message):
+    c = await functional_man.close_status(message.from_user.id)
+    await message.answer(c)
+
+
+@dp.message(Command(commands="ready_for_buyer"))
+async def process_open_state_command(message: Message):
+    c = await functional_man.open_state(message.from_user.id)
+    await message.answer(c)
+
+
+@dp.message(Command(commands="manager"))
+async def process_search_manager_command(message: Message):
+    text = await functional_buy.distribution(message.from_user.id)
+    await message.answer(text)
+# этот хэндлер срабатывает на остальные сообщения
+# @dp.message()
+# async def managers(message: Message):
 
 
 async def main():
